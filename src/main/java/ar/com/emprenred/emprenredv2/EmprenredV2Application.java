00fbaf2f5ed6410bf86483ccc9ab4ac7d87db600
unknown <feliperebolledo@outlook.com>
2022-06-06 15:10:20 -0300

    Se clona repo anterior

---
+++ src/main/java/ar/com/emprenred/emprenredv2/EmprenredV2Application.java
@@ -0,0 +1,13 @@
+package ar.com.emprenred.emprenredv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprenredV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmprenredV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ar/com/emprenred/emprenredv2/EmprenredV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.emprenred.emprenredv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprenredV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

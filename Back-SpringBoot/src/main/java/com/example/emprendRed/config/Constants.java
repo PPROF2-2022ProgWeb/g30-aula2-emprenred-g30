c1543efd77a82266f1a5ee0394a16ea297e6b9e8
Celeste Miranda <celeste24@gmail.com>
2022-10-30 22:35:40 -0300

    se agrego swagger

---
+++ Back-SpringBoot/src/main/java/com/example/emprendRed/config/Constants.java
@@ -0,0 +1,19 @@
+package com.example.emprendRed.config;
+
+public class Constants {
+    public final static String AUTHORIZATION_HEADER = "Authorization";
+    public static final String JWT = "JWT";
+    public static final String HEADER = "HEADER";
+    public static final String PATH = "com.example.emprendRed.controller";
+
+    public static final String TITLE = "EMPRENRED";
+    public static final String VERSION = "v1";
+    public static final String DESCRIPTION = "EmprendRed";
+    public static final String TERMS_OF_SERVICE = "Términos y condiciones";
+
+    public static final String CONTACT_NAME = "emprendred";
+    public static final String CONTACT_URL = "www.emprendred.com.ar";
+    public static final String CONTACT_EMAIL = "enprendred@gmail.com";
+    public static final String LICENCE = "Licencia de API";
+    public static final String LICENCE_URL = "www.licence.com";
+}

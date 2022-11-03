77d778501823f415d91f56c209bda95e62849442
rebofel <feliperebolledo@outlook.com>
2022-11-03 18:52:54 -0300

    Vista usuario , modificar password (sin endpoint, solo recibe)

--- emprenred-front/src/app/app.module.ts
+++ emprenred-front/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LOCALE_ID } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';

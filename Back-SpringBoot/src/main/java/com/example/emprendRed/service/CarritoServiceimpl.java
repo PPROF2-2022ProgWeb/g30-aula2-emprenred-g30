2e58fd3c1acbdbcfca5ca47ef34998b8a2f3ecf6
Celeste Miranda <celeste24@gmail.com>
2022-11-07 01:06:15 -0300

    Se hizo funcion de carrito

--- Back-SpringBoot/src/main/java/com/example/emprendRed/service/CarritoServiceimpl.java
+++ Back-SpringBoot/src/main/java/com/example/emprendRed/service/CarritoServiceimpl.java
@@ -3,6 +3,10 @@ package com.example.emprendRed.service;
 import java.util.List;
 import java.util.Optional;
 
+import com.example.emprendRed.exceptions.BadRequestException;
+import com.example.emprendRed.model.Persona;
+import com.example.emprendRed.model.Productos;
+import com.example.emprendRed.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,6 +21,12 @@ public class CarritoServiceimpl implements CarritoService {
 
 	@Autowired
 	private CarritoRepositorio carritoRepositorio;
+
+	@Autowired
+	private Utils utils;
+
+	@Autowired
+	private ProductosService productosService;
 	
 	@Override
 	@Transactional(readOnly=true)
@@ -49,15 +59,53 @@ public class CarritoServiceimpl implements CarritoService {
 		carritoRepositorio.deleteById(id);
 	}
 	@Override
-	public List<Carrito> searchNativo(String filtro) throws Exception {
+	public List<Carrito> searchNativo(Long filtro) throws Exception {
 		try {
-			List<Carrito> carritos = carritoRepositorio.searchNativo(filtro);
+			List<Carrito> carritos = carritoRepositorio.searchByPerson(filtro);
 		return carritos;
 		}catch (Exception e) {
 			throw new Exception(e.getMessage());
 		}
 		
 	}
+
+	@Override
+	@Transactional
+	public void addProducto(Long productoId) throws Exception {
+		Persona persona = utils.getPersonContext();
+		Carrito carrito = searchNativo(persona.getId()).size()==0? null:searchNativo(persona.getId()).get(0);
+		Productos producto =  productosService.findById(productoId).orElseThrow(()-> new BadRequestException("producto invalido"));
+		if (carrito== null){
+			carrito = new Carrito();
+			carrito.setPersona(persona);
+		}
+
+		carrito.getProductos().add(producto);
+
+		carritoRepositorio.save(carrito);
+		updatePrecioCarrito(persona.getId());
+	}
+
+	@Override
+	@Transactional
+	public void deleteProductoToCarrito(List<Long> productoIds) throws Exception {
+		Persona persona = utils.getPersonContext();
+		Carrito carrito = searchNativo(persona.getId()).size()==0? null:searchNativo(persona.getId()).get(0);
+
+		carritoRepositorio.deleteProducts(carrito.getId(),productoIds);
+		updatePrecioCarrito(persona.getId());
+
+
+	}
+	private void updatePrecioCarrito (Long personaId) throws Exception {
+		Double precioCarrito = carritoRepositorio.getPrecioByPersona(personaId).orElse(0.0);
+
+		Carrito carrito = searchNativo(personaId).get(0);
+
+		carrito.setPrecio(precioCarrito);
+		carritoRepositorio.save(carrito);
+	}
+
 }
 
 

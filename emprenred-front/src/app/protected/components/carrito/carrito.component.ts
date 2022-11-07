e9a77f4c1d3b84791e459ca7c55551e30c2dce7a
dakoo096 <noelia_cabral96@hotmail.com>
2022-11-07 20:39:10 -0300

    se agrega template carrito,css de tabla productos y tabla producto

--- emprenred-front/src/app/protected/components/carrito/carrito.component.ts
+++ emprenred-front/src/app/protected/components/carrito/carrito.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Producto, TipoProducto } from '../../../marketplace/interfaces/producto.interface';
+import { MarketplaceService } from '../../../marketplace/services/marketplace.service';
 
 @Component({
   selector: 'app-carrito',
@@ -7,9 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarritoComponent implements OnInit {
 
-  constructor() { }
+
+  categoria = "";
+productos: Producto[] = [];
+
+  constructor(private activatedRoute : ActivatedRoute,
+             private marketplaceService: MarketplaceService) { }
 
   ngOnInit(): void {
-  }
 
+    this.marketplaceService.listarProductos()
+    .subscribe( (productos) => {
+    
+      this.productos = productos.data; 
+      console.log("Query OK");  
+
+      console.log(this.productos)
+    
+     
+    }, (err) => {
+
+      this.productos= [];
+      console.log("error")
+    })
+  
+
+
+
+
+    this.activatedRoute.params.subscribe(({categoria})=>{
+   
+      
+      if(categoria != ''){
+        this.categoria = categoria; 
+      }
+  
+    });
+   
+
+}
+
+total(){
+  let sum=0;
+  this.productos.forEach(producto => {
+    sum += 1 * producto.precio
+  });
+  return sum;
+}
+ /*en suma cambiar el 1 por producto cantidad cuando este ok*/ 
 }
